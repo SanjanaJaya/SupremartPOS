b4e2deec12b584f3997e33543092d7e9880c7d5f
Gaveen Ranasinghe <137697325+Gaveenr@users.noreply.github.com>
2024-12-20 21:44:20 +0530

    Gaveen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,15 +30,15 @@
         {
             Shehara = new Button();
             button1 = new Button();
+            checkBox1 = new CheckBox();
             SuspendLayout();
             // 
             // Shehara
             // 
             Shehara.BackColor = SystemColors.MenuHighlight;
-            Shehara.Location = new Point(345, 354);
-            Shehara.Margin = new Padding(6, 6, 6, 6);
+            Shehara.Location = new Point(186, 166);
             Shehara.Name = "Shehara";
-            Shehara.Size = new Size(139, 49);
+            Shehara.Size = new Size(75, 23);
             Shehara.TabIndex = 0;
             Shehara.Text = "Shehara";
             Shehara.UseVisualStyleBackColor = false;
@@ -46,31 +46,45 @@
             // 
             // button1
             // 
-            button1.Location = new Point(811, 385);
+            button1.Location = new Point(437, 180);
+            button1.Margin = new Padding(2, 1, 2, 1);
             button1.Name = "button1";
-            button1.Size = new Size(312, 178);
+            button1.Size = new Size(168, 83);
             button1.TabIndex = 1;
             button1.Text = "Pakaya";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click_1;
             // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(703, 180);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1894, 1009);
+            ClientSize = new Size(1020, 473);
+            Controls.Add(checkBox1);
             Controls.Add(button1);
             Controls.Add(Shehara);
-            Margin = new Padding(4, 2, 4, 2);
+            Margin = new Padding(2, 1, 2, 1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button Shehara;
         private Button button1;
+        private CheckBox checkBox1;
     }
 }
