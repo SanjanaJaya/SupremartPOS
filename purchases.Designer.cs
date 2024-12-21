d2d0ddfb42f9f2519bee5e661715f16da7b923dd
Sanjana Jayasooriya <135824601+SanjanaJaya@users.noreply.github.com>
2024-12-21 18:08:08 +0530

    db

--- purchases.Designer.cs
+++ purchases.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace SuprememartPOS
+﻿
+namespace SuprememartPOS
 {
     partial class purchases
     {
@@ -29,41 +30,43 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(purchases));
-            dataGridViewPurchase = new DataGridView();
-            purchase = new Label();
+            dataGridViewPurchases = new DataGridView();
+            purchaseslabel = new Label();
             pictureBox1 = new PictureBox();
-            dataGridView1 = new DataGridView();
-            label1 = new Label();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            addcustomer = new Button();
-            button1 = new Button();
+            productlistlabel = new Label();
+            currentorderlabel = new Label();
+            searchbar = new TextBox();
+            search = new Button();
+            addtoorder = new Button();
             label3 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dataGridViewPurchase).BeginInit();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPurchases).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // dataGridViewPurchase
+            // dataGridViewPurchases
             // 
-            dataGridViewPurchase.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewPurchase.Location = new Point(16, 98);
-            dataGridViewPurchase.Margin = new Padding(2, 1, 2, 1);
-            dataGridViewPurchase.Name = "dataGridViewPurchase";
-            dataGridViewPurchase.RowHeadersWidth = 82;
-            dataGridViewPurchase.Size = new Size(1494, 302);
-            dataGridViewPurchase.TabIndex = 5;
+            dataGridViewPurchases.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewPurchases.Location = new Point(16, 98);
+            dataGridViewPurchases.Margin = new Padding(2, 1, 2, 1);
+            dataGridViewPurchases.Name = "dataGridViewPurchases";
+            dataGridViewPurchases.RowHeadersWidth = 82;
+            dataGridViewPurchases.Size = new Size(1494, 302);
+            dataGridViewPurchases.TabIndex = 5;
+            dataGridViewPurchases.CellContentClick += dataGridViewPurchase_CellContentClick;
             // 
-            // purchase
+            // purchaseslabel
             // 
-            purchase.AutoSize = true;
-            purchase.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            purchase.Location = new Point(16, 12);
-            purchase.Margin = new Padding(2, 0, 2, 0);
-            purchase.Name = "purchase";
-            purchase.Size = new Size(194, 50);
-            purchase.TabIndex = 6;
-            purchase.Text = "Purchases";
+            purchaseslabel.AutoSize = true;
+            purchaseslabel.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            purchaseslabel.Location = new Point(16, 12);
+            purchaseslabel.Margin = new Padding(2, 0, 2, 0);
+            purchaseslabel.Name = "purchaseslabel";
+            purchaseslabel.Size = new Size(194, 50);
+            purchaseslabel.TabIndex = 6;
+            purchaseslabel.Text = "Purchases";
+            purchaseslabel.Click += purchase_Click;
             // 
             // pictureBox1
             // 
@@ -74,75 +77,68 @@
             pictureBox1.TabIndex = 9;
             pictureBox1.TabStop = false;
             // 
-            // dataGridView1
+            // productlistlabel
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(16, 516);
-            dataGridView1.Margin = new Padding(2, 1, 2, 1);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 82;
-            dataGridView1.Size = new Size(1494, 302);
-            dataGridView1.TabIndex = 10;
+            productlistlabel.AutoSize = true;
+            productlistlabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            productlistlabel.Location = new Point(19, 55);
+            productlistlabel.Name = "productlistlabel";
+            productlistlabel.Size = new Size(156, 37);
+            productlistlabel.TabIndex = 11;
+            productlistlabel.Text = "Product List";
+            productlistlabel.Click += label1_Click;
             // 
-            // label1
+            // currentorderlabel
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(19, 55);
-            label1.Name = "label1";
-            label1.Size = new Size(156, 37);
-            label1.TabIndex = 11;
-            label1.Text = "Product List";
-            label1.Click += label1_Click;
+            currentorderlabel.AutoSize = true;
+            currentorderlabel.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            currentorderlabel.Location = new Point(19, 472);
+            currentorderlabel.Name = "currentorderlabel";
+            currentorderlabel.Size = new Size(180, 37);
+            currentorderlabel.TabIndex = 12;
+            currentorderlabel.Text = "Current Order";
             // 
-            // label2
+            // searchbar
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(19, 472);
-            label2.Name = "label2";
-            label2.Size = new Size(180, 37);
-            label2.TabIndex = 12;
-            label2.Text = "Current Order";
+            searchbar.BackColor = Color.Silver;
+            searchbar.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            searchbar.Location = new Point(629, 49);
+            searchbar.Name = "searchbar";
+            searchbar.Size = new Size(374, 43);
+            searchbar.TabIndex = 13;
+            searchbar.TextChanged += textBox1_TextChanged;
             // 
-            // textBox1
+            // search
             // 
-            textBox1.BackColor = Color.Silver;
-            textBox1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(629, 49);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(374, 43);
-            textBox1.TabIndex = 13;
+            search.BackColor = Color.Transparent;
+            search.BackgroundImage = (Image)resources.GetObject("search.BackgroundImage");
+            search.FlatAppearance.BorderSize = 0;
+            search.FlatStyle = FlatStyle.Flat;
+            search.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            search.ForeColor = Color.White;
+            search.Location = new Point(1025, 53);
+            search.Margin = new Padding(2, 1, 2, 1);
+            search.Name = "search";
+            search.Size = new Size(42, 37);
+            search.TabIndex = 23;
+            search.UseVisualStyleBackColor = false;
+            search.Click += addcustomer_Click;
             // 
-            // addcustomer
+            // addtoorder
             // 
-            addcustomer.BackColor = Color.Transparent;
-            addcustomer.BackgroundImage = (Image)resources.GetObject("addcustomer.BackgroundImage");
-            addcustomer.FlatAppearance.BorderSize = 0;
-            addcustomer.FlatStyle = FlatStyle.Flat;
-            addcustomer.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            addcustomer.ForeColor = Color.White;
-            addcustomer.Location = new Point(1025, 53);
-            addcustomer.Margin = new Padding(2, 1, 2, 1);
-            addcustomer.Name = "addcustomer";
-            addcustomer.Size = new Size(42, 37);
-            addcustomer.TabIndex = 23;
-            addcustomer.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.Transparent;
-            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(611, 419);
-            button1.Margin = new Padding(2, 1, 2, 1);
-            button1.Name = "button1";
-            button1.Size = new Size(254, 54);
-            button1.TabIndex = 24;
-            button1.UseVisualStyleBackColor = false;
+            addtoorder.BackColor = Color.Transparent;
+            addtoorder.BackgroundImage = (Image)resources.GetObject("addtoorder.BackgroundImage");
+            addtoorder.FlatAppearance.BorderSize = 0;
+            addtoorder.FlatStyle = FlatStyle.Flat;
+            addtoorder.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            addtoorder.ForeColor = Color.White;
+            addtoorder.Location = new Point(611, 419);
+            addtoorder.Margin = new Padding(2, 1, 2, 1);
+            addtoorder.Name = "addtoorder";
+            addtoorder.Size = new Size(254, 54);
+            addtoorder.TabIndex = 24;
+            addtoorder.UseVisualStyleBackColor = false;
+            addtoorder.Click += button1_Click;
             // 
             // label3
             // 
@@ -154,40 +150,56 @@
             label3.TabIndex = 25;
             label3.Text = "Total:";
             // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(16, 519);
+            dataGridView1.Margin = new Padding(2, 1, 2, 1);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            dataGridView1.Size = new Size(1494, 302);
+            dataGridView1.TabIndex = 26;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
             // purchases
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(label3);
-            Controls.Add(button1);
-            Controls.Add(addcustomer);
-            Controls.Add(textBox1);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(dataGridView1);
+            Controls.Add(label3);
+            Controls.Add(addtoorder);
+            Controls.Add(search);
+            Controls.Add(searchbar);
+            Controls.Add(currentorderlabel);
+            Controls.Add(productlistlabel);
             Controls.Add(pictureBox1);
-            Controls.Add(purchase);
-            Controls.Add(dataGridViewPurchase);
+            Controls.Add(purchaseslabel);
+            Controls.Add(dataGridViewPurchases);
             Name = "purchases";
             Size = new Size(1540, 922);
-            ((System.ComponentModel.ISupportInitialize)dataGridViewPurchase).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPurchases).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
 
-        private DataGridView dataGridViewPurchase;
-        private Label purchase;
+        private DataGridView dataGridViewPurchases;
+        private Label purchaseslabel;
         private PictureBox pictureBox1;
-        private DataGridView dataGridView1;
-        private Label label1;
-        private Label label2;
-        private TextBox textBox1;
-        private Button addcustomer;
-        private Button button1;
+        private Label productlistlabel;
+        private Label currentorderlabel;
+        private TextBox searchbar;
+        private Button search;
+        private Button addtoorder;
         private Label label3;
+        private DataGridView dataGridView1;
     }
 }
--- purchases.cs
+++ purchases.cs
@@ -21,5 +21,40 @@ namespace SuprememartPOS
         {
 
         }
+
+        private void data_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridViewPurchase_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addcustomer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void purchase_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
