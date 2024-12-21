0a857b888a324bfe72e984d0fa7977cab61d5608
Sanjana Jayasooriya <135824601+SanjanaJaya@users.noreply.github.com>
2024-12-21 11:50:02 +0530

    Suupplier

--- supplier.Designer.cs
+++ supplier.Designer.cs
@@ -32,6 +32,17 @@
             dataGridViewSuppliers = new DataGridView();
             supplir = new Label();
             pictureBox1 = new PictureBox();
+            button2 = new Button();
+            button1 = new Button();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            addcustomer = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridViewSuppliers).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
@@ -43,7 +54,7 @@
             dataGridViewSuppliers.Margin = new Padding(2, 1, 2, 1);
             dataGridViewSuppliers.Name = "dataGridViewSuppliers";
             dataGridViewSuppliers.RowHeadersWidth = 82;
-            dataGridViewSuppliers.Size = new Size(1494, 749);
+            dataGridViewSuppliers.Size = new Size(1494, 449);
             dataGridViewSuppliers.TabIndex = 4;
             // 
             // supplir
@@ -67,10 +78,139 @@
             pictureBox1.TabIndex = 6;
             pictureBox1.TabStop = false;
             // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(909, 658);
+            button2.Name = "button2";
+            button2.Size = new Size(254, 54);
+            button2.TabIndex = 29;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(909, 733);
+            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Name = "button1";
+            button1.Size = new Size(254, 54);
+            button1.TabIndex = 28;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ScrollBar;
+            textBox4.Font = new Font("Segoe UI", 15.75F);
+            textBox4.Location = new Point(238, 798);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(350, 35);
+            textBox4.TabIndex = 27;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ScrollBar;
+            textBox3.Font = new Font("Segoe UI", 15.75F);
+            textBox3.Location = new Point(238, 730);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(350, 35);
+            textBox3.TabIndex = 26;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F);
+            label5.Location = new Point(60, 798);
+            label5.Name = "label5";
+            label5.Size = new Size(135, 30);
+            label5.TabIndex = 25;
+            label5.Text = "NIC Number:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F);
+            label4.Location = new Point(60, 730);
+            label4.Name = "label4";
+            label4.Size = new Size(172, 30);
+            label4.TabIndex = 24;
+            label4.Text = "Contact Number:";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ScrollBar;
+            textBox2.Font = new Font("Segoe UI", 15.75F);
+            textBox2.Location = new Point(238, 655);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(350, 35);
+            textBox2.TabIndex = 23;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F);
+            label3.Location = new Point(60, 655);
+            label3.Name = "label3";
+            label3.Size = new Size(117, 30);
+            label3.TabIndex = 22;
+            label3.Text = "Last Name:";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ScrollBar;
+            textBox1.Font = new Font("Segoe UI", 15.75F);
+            textBox1.Location = new Point(238, 588);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(350, 35);
+            textBox1.TabIndex = 21;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F);
+            label2.Location = new Point(60, 588);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 30);
+            label2.TabIndex = 20;
+            label2.Text = "First Name:";
+            // 
+            // addcustomer
+            // 
+            addcustomer.BackColor = Color.Transparent;
+            addcustomer.BackgroundImage = (Image)resources.GetObject("addcustomer.BackgroundImage");
+            addcustomer.FlatAppearance.BorderSize = 0;
+            addcustomer.FlatStyle = FlatStyle.Flat;
+            addcustomer.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            addcustomer.ForeColor = Color.White;
+            addcustomer.Location = new Point(909, 588);
+            addcustomer.Margin = new Padding(2, 1, 2, 1);
+            addcustomer.Name = "addcustomer";
+            addcustomer.Size = new Size(254, 54);
+            addcustomer.TabIndex = 19;
+            addcustomer.UseVisualStyleBackColor = false;
+            // 
             // supplier
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(addcustomer);
             Controls.Add(pictureBox1);
             Controls.Add(supplir);
             Controls.Add(dataGridViewSuppliers);
@@ -87,5 +227,16 @@
         private DataGridView dataGridViewSuppliers;
         private Label supplir;
         private PictureBox pictureBox1;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label5;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Button addcustomer;
     }
 }
