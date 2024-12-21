b3a78226d0642b62844474050d98276419162e0d
Gaveen Ranasinghe <137697325+Gaveenr@users.noreply.github.com>
2024-12-21 12:05:43 +0530

    employee

--- employees.Designer.cs
+++ employees.Designer.cs
@@ -32,18 +32,27 @@
             dataGridViewEmplyoee = new DataGridView();
             employee = new Label();
             pictureBox1 = new PictureBox();
+            empnic = new TextBox();
+            empcon = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            emplname = new TextBox();
+            label3 = new Label();
+            empfname = new TextBox();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridViewEmplyoee).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // dataGridViewEmplyoee
             // 
+            dataGridViewEmplyoee.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             dataGridViewEmplyoee.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewEmplyoee.Location = new Point(16, 73);
             dataGridViewEmplyoee.Margin = new Padding(2, 1, 2, 1);
             dataGridViewEmplyoee.Name = "dataGridViewEmplyoee";
             dataGridViewEmplyoee.RowHeadersWidth = 82;
-            dataGridViewEmplyoee.Size = new Size(1494, 749);
+            dataGridViewEmplyoee.Size = new Size(1494, 449);
             dataGridViewEmplyoee.TabIndex = 6;
             // 
             // employee
@@ -66,10 +75,94 @@
             pictureBox1.TabIndex = 8;
             pictureBox1.TabStop = false;
             // 
+            // empnic
+            // 
+            empnic.BackColor = SystemColors.ScrollBar;
+            empnic.Font = new Font("Segoe UI", 15.75F);
+            empnic.Location = new Point(237, 799);
+            empnic.Name = "empnic";
+            empnic.Size = new Size(350, 35);
+            empnic.TabIndex = 21;
+            // 
+            // empcon
+            // 
+            empcon.BackColor = SystemColors.ScrollBar;
+            empcon.Font = new Font("Segoe UI", 15.75F);
+            empcon.Location = new Point(237, 730);
+            empcon.Name = "empcon";
+            empcon.Size = new Size(350, 35);
+            empcon.TabIndex = 20;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F);
+            label5.Location = new Point(59, 799);
+            label5.Name = "label5";
+            label5.Size = new Size(135, 30);
+            label5.TabIndex = 19;
+            label5.Text = "NIC Number:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F);
+            label4.Location = new Point(59, 733);
+            label4.Name = "label4";
+            label4.Size = new Size(172, 30);
+            label4.TabIndex = 18;
+            label4.Text = "Contact Number:";
+            // 
+            // emplname
+            // 
+            emplname.BackColor = SystemColors.ScrollBar;
+            emplname.Font = new Font("Segoe UI", 15.75F);
+            emplname.Location = new Point(237, 652);
+            emplname.Name = "emplname";
+            emplname.Size = new Size(350, 35);
+            emplname.TabIndex = 17;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F);
+            label3.Location = new Point(59, 657);
+            label3.Name = "label3";
+            label3.Size = new Size(117, 30);
+            label3.TabIndex = 16;
+            label3.Text = "Last Name:";
+            // 
+            // empfname
+            // 
+            empfname.BackColor = SystemColors.ScrollBar;
+            empfname.Font = new Font("Segoe UI", 15.75F);
+            empfname.Location = new Point(237, 585);
+            empfname.Name = "empfname";
+            empfname.Size = new Size(350, 35);
+            empfname.TabIndex = 15;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F);
+            label2.Location = new Point(59, 585);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 30);
+            label2.TabIndex = 14;
+            label2.Text = "First Name:";
+            // 
             // employees
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(empnic);
+            Controls.Add(empcon);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(emplname);
+            Controls.Add(label3);
+            Controls.Add(empfname);
+            Controls.Add(label2);
             Controls.Add(pictureBox1);
             Controls.Add(employee);
             Controls.Add(dataGridViewEmplyoee);
@@ -86,5 +179,13 @@
         private DataGridView dataGridViewEmplyoee;
         private Label employee;
         private PictureBox pictureBox1;
+        private TextBox empnic;
+        private TextBox empcon;
+        private Label label5;
+        private Label label4;
+        private TextBox emplname;
+        private Label label3;
+        private TextBox empfname;
+        private Label label2;
     }
 }
